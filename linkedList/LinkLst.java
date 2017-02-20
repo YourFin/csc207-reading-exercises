a0d2a96fd7c824e67d36267955d9b8d5aab3ed6e
YourFin <d.junkpobox+github@gmail.com>
2017-02-20 10:58:40 -0600

    addade linked list

---
+++ linkedList/LinkLst.java
@@ -0,0 +1,61 @@
+public class LnkList {
+	private static class Node {
+		public Object oo;
+		public node next;
+		public Node(Object oo, Node nn) {
+			this.oo = oo;
+			this.next = next;
+		}
+		public Node(Object oo) {
+			this.oo = oo;
+			this.next = null;
+		}
+	}
+
+	private Node firstNode;
+	public LnkList (Object oo) {
+		firstNode = new Node(oo);
+	}
+
+	public LnkList () {
+		firstNode = null;
+	}
+
+	public void addFirst (Object oo) {
+		firstNode = new Node(oo, firstNode);
+	}
+
+	public int size() {
+		return sizeRec(this.firstNode, 0);
+	}
+
+	private int sizeRec(Node nn, int current) {
+		if(nn == null) {
+			return current;
+		} else {
+			return sizeRec(nn.next, current + 1);
+		}
+	}
+
+	public void addLast(Object object) {
+		if( firstNode == null) {
+			firstNode = new Node(object);
+			return;
+		}
+		addLastRec(object, firstNode);
+	}
+
+	private void addLastRec(Object object, Node nn) {
+		if(nn.next == null)
+		{
+			nn.next = new Node(object);
+		} else {
+			addLastRec(object, nn.next);
+		}
+	}
+}
+				
+
+	}
+
+}
